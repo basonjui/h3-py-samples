6b1b096dfa6e6b0216d1b3e9e44a31ccb531ab4a
Quan Bui <basonjui@gmail.com>
2023-06-10 18:54:28 +0700

    Add pg_client to simplify database connection

---
+++ pg_client.py
@@ -0,0 +1,37 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+class PgClient:
+    def __init__(self, database="spatial_dwh"):
+        load_dotenv(override=True)
+
+        self.config = {
+            "host": os.environ.get("POSTGRES_HOST", None),
+            "port": os.environ.get("POSTGRES_PORT", None),
+            "user": os.environ.get("POSTGRES_USERNAME", None),
+            "password": os.environ.get("POSTGRES_PASSWORD", None),
+            "database": database,
+        }
+        
+        try: 
+            self.conn = psycopg2.connect(**self.config)
+            self.cur = self.conn.cursor()
+            print(f"Connected to PostgreSQL database '{self.config['database']}'")
+        except(Exception, psycopg2.DatabaseError) as error:
+            raise Exception(error)
+
+    def cursor(self):
+        return self.cur
+    
+    def execute(self, query):
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def fetchall(self, query):
+        self.cur.execute(query)
+        return self.cur.fetchall()
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
